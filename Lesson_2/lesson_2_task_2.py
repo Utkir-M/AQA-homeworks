0af1ddf99d537dcf4ed3a6718133bb6a7a5b7552
Utkir <elriu89@gmail.com>
2023-07-01 20:52:33 +0500

    2 - задача

--- Lesson_2/lesson_2_task_2.py
+++ Lesson_2/lesson_2_task_2.py
@@ -1,1 +1,7 @@
 # Создайте функцию is_year_leap, принимающую 1 аргумент — год (число) — и возвращающую True, если год високосный, и False — иначе.
+is_year_leap = input("Введите год: ")
+    
+if (int(is_year_leap) % 4 == 0):
+    print("год ", is_year_leap, ":", True)
+else:
+    print("год ", is_year_leap, ":", False)
