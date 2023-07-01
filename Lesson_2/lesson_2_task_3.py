77e91c4bb8ae9ca0e715fe96d7c41e48f481edb3
Utkir <elriu89@gmail.com>
2023-07-01 21:30:22 +0500

    Задача 3

---
+++ Lesson_2/lesson_2_task_3.py
@@ -0,0 +1,7 @@
+#1-й вариант
+#square_str = input("Введите одну сторону квадрата: ")
+#square = int(square_str)
+# 2-й вариант:
+square = int(input("Введите одну сторону квадрата: "))
+
+print('Площадь квадрата =', square * square)
